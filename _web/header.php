ff7abbda54cacf973e4f5ef97a60e7cd4a22802f
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-07-25 16:34:51 +0530

    HEADER SET

--- _web/header.php
+++ _web/header.php
@@ -9,33 +9,11 @@
 
 <body>
     <div class="boxed_wrapper ltr">
-        <!-- Main header-->
         <header class="main-header header-style-one">
-            <!--Start Header Top-->
             <div class="header-top">
                 <div class="auto-container">
                     <div class="outer-box">
                         <div class="header-top-left">
-                            <!-- <div class="social-link-box-style1">
-                                <div class="icon">
-                                    <span class="icon-share"></span>
-                                </div>
-                                <p>Follow</p>
-                                <ul class="clearfix">
-                                    <li>
-                                        <a href="https://www.facebook.com/crescentpublicschoolpalampur"
-                                            target="_blank"><i class="icon-facebook-app-symbol"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="https://www.instagram.com/crescentpublicschoolbanuri/"
-                                            target="_blank"><i class="icon-instagram"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="https://www.youtube.com/channel/UCsWfbcRc22RMD6esXmKRC0w"
-                                            target="_blank"><i class="icon-youtube"></i></a>
-                                    </li>
-                                </ul>
-                            </div> -->
                             <div class="info-box">
                                 <span class="icon-info"></span>
                                 <p>School Code: 40047</p>
@@ -44,10 +22,6 @@
                                 <span class="icon-info"></span>
                                 <p>Affiliation No: 530027</p>
                             </div>
-                            <!-- <div class="info-box">
-                                <span class="icon-info"></span>
-                                <p>UDISE Code: 06070106847</p>
-                            </div> -->
                             <div class="info-box">
                                 <span class="icon-info"></span>
                                 <a href="cbsemandate.php">
@@ -62,7 +36,6 @@
                                     </a></p>
                             </div>
                         </div>
-
                         <div class="header-top-right">
                             <div class="quick-link-box">
                                 <div class="link-box">
@@ -76,55 +49,9 @@
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
-            <!--End Header Top-->
-
-
-
-            <!-- Side icon area start-->
-            <!-- <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
-            <script>
-                function openNav() {
-                    document.getElementById("mySidenav").style.width = "350px";
-                    document.getElementById("main").style.marginLeft = "370px";
-                }
-                function closeNav() {
-                    document.getElementById("mySidenav").style.width = "0";
-                    document.getElementById("main").style.marginLeft = "0";
-                }
-            </script>
-            <div class="icon-bar-right" onclick="openNav()">
-                <a href="#"><img src="/assets/images/bgEnquiry.png" class="side-bar-image" /></a>
-            </div>
-            <div id="mySidenav" class="sidenav">
-                <a href="javascript:void(0)" class="closebtn1" onclick="closeNav()">&times;</a>
-                <div class="row">
-                    <div class="col-md-12" style="padding:0px 40px">
-                        <h4>Online Enquiry</h4>
-                        <iframe style="height:460px;" width="100%"
-                            src="https://curtina.in/_EazySchool/FormsWeb/frmEnquiry.aspx?AppId=crescent">
-
-                        </iframe>
-                    </div>
-                </div>
-            </div> -->
-            <!--
-    admission open button close
--->
-            <!--icon bar left start-->
-            <!-- <div class="icon-bar-left">
-                <a href="https://www.facebook.com/Crescent-School-Banuri-Palampur-CBSE-Board-317253068398836"
-                    class="facebook" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
-                <a href="https://www.youtube.com/channel/UCsWfbcRc22RMD6esXmKRC0w" class="youtube" target="_blank"><i
-                        class="fa-brands fa-youtube"></i></a>
-                <a href="https://www.instagram.com/crescentpublicschoolbanuri/" class="instagram" target="_blank"><i
-                        class="fa-brands fa-instagram"></i></a>
-            </div> -->
-            <!--icon bar left end-->
-            <!--Start Header-->
             <div class="header-style7">
                 <div class="container">
                     <div class="outer-box">
@@ -137,7 +64,6 @@
                         </div>
                         <div class="header-style7__middle">
                             <div class="nav-outer style1 clearfix">
-
                                 <div class="mobile-nav-toggler">
                                     <div class="inner">
                                         <span class="icon-bar"></span>
@@ -157,24 +83,17 @@
                                                     <li><a href="principal-message.php">Principal's Message</a></li>
                                                     <li><a href="our-objective.php">Our Objectives</a></li>
                                                     <li><a href="our_achievements.php"> Our Achievements</a></li>
-                                                    <!-- 
-                                                    <li><a href="eminent.php">Eminent Guests at school</a></li>
-                                                    <li><a href="alumni.php">Alumni</a></li> -->
                                                     <li><a href="mandatory_disclosure.php">Mandatory Disclosure</a></li>
-                                                    <!-- <li><a href="schoolmap.php">distance From School</a></li> -->
                                                 </ul>
                                             </li>
                                             <li class="dropdown"><a href="#">Academic</a>
                                                 <ul>
                                                     <li><a href="academic_features.php">Academic Features</a></li>
-
                                                     <li><a href="unique_methodology.php">Unique Methodology</a></li>
                                                     <li><a href="co_curricular_activities.php">Co-Curricular
                                                             Activities</a></li>
                                                     <li><a href="activities.php">Activities</a></li>
-                                                    <!-- <li><a href="pastoralcare.php">Pastoral Care</a></li> -->
                                                     <li><a href="faculty.php">Faculty</a></li>
-
                                                     <li><a href="activity_calender.php">Activity Calender</a></li>
                                                 </ul>
                                             </li>
@@ -186,13 +105,9 @@
                                                     <li><a href="atal.php">Atal Tinkering Lab</a></li>
                                                     <li><a href="robotics.php">Robotics</a></li>
                                                     <li><a href="sports_games.php">Sports & Games</a></li>
-                                                    <!-- <li><a href="hostel_facilities.php">Hostel Facilities</a></li> -->
                                                     <li><a href="transport.php">Transport</a></li>
                                                     <li><a href="tclp.php">coaching classes</a></li>
-                                                    <!-- <li><a href="exam_coaching.php">The Crescent Learning Programme</a>
-                                                    </li> -->
                                                     <li><a href="careercounselling.php">Career Counselling</a></li>
-                                                    <!-- <li><a href="coding.php"> Coding</a></li> -->
                                                     <li><a href="skating-rink.php"> Skating Rink</a></li>
                                                     <li><a href="table-tennis.php"> Table Tennis</a></li>
                                                 </ul>
@@ -202,7 +117,6 @@
                                                     <li><a href="value-inculcation.php">Value Inculcation</a></li>
                                                     <li><a href="codeofconduct.php">Code of Conduct</a></li>
                                                     <li><a href="dresscode.php">Dress Code</a></li>
-                                                    <!-- <li><a href="attendance.php">Attendance</a></li> -->
                                                     <li><a href="rulesofdiscipline.php">Rules of Discipline</a></li>
                                                 </ul>
                                             </li>
@@ -211,10 +125,8 @@
                                 </nav>
                             </div>
                         </div>
-
                         <div class="header-style7__right">
                             <ul class="clearfix">
-
                             </ul>
                             <div class="btns-box">
                                 <a class="btn-one" href="https://curtina.in/mkk">
@@ -225,42 +137,18 @@
                     </div>
                 </div>
             </div>
-            <!--End header-->
-
-            <!--Sticky Header-->
-            <!-- <div class="sticky-header">
-                <div class="container">
-                    <div class="sticky-header__inner clearfix">
-                        <div class="logo float-left">
-                            <a href="index.php" class="img-responsive">
-                                <img src="/assets/images/resources/logo-3.png" alt="" title="">
-                            </a>
-                        </div>
-                        <div class="right-col float-right">
-                            <nav class="main-menu clearfix">
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-            <!--End Sticky Header-->
-
-            <!-- Mobile Menu  -->
             <div class="mobile-menu">
                 <div class="menu-backdrop"></div>
                 <div class="close-btn"><span class="icon fa fa-times-circle"></span></div>
                 <nav class="menu-box">
                     <div class="nav-logo">
                         <a href="index.php">
-                            <img src="/assets/images/logo-dark.png" alt="" title="" style="    background: #fff;
-    padding: 10px;
-    border-radius: 10px;">
+                            <img src="/assets/images/logo-dark.png" alt="" title=""
+                                style="background: #fff; padding: 10px; border-radius: 10px;">
                         </a>
                     </div>
                     <div class="menu-outer">
-                        <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
                     </div>
-                    <!--Social Links-->
                     <div class="social-links">
                         <ul class="clearfix">
                             <li><a href="https://www.facebook.com/mkkschoolpanipat"><span
@@ -270,5 +158,4 @@
                     </div>
                 </nav>
             </div>
-            <!-- End Mobile Menu -->
         </header>
