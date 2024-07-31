e7f660f787feadf09e4fd7e0cb532222171de6d7
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-07-31 11:06:31 +0530

    principal message added

--- about-us.php
+++ about-us.php
@@ -3,7 +3,7 @@ include('_web/header.php');
 include('_web/css.php');
 ?>
 
-<section class="breadcrumb-style7-area">
+<!-- <section class="breadcrumb-style7-area">
    
     <div class="container">
         <div class="row">
@@ -17,7 +17,7 @@ include('_web/css.php');
             </div>
         </div>
     </div>
-</section>
+</section> -->
 
 <section class="about-style1-area">
     <div class="container">
@@ -30,74 +30,81 @@ include('_web/css.php');
             <div class="col-xl-12">
                 <div class="about-style1__inner">
                     <div class="academics-overview-style1-content academics-overview-style1-content--in-style2">
-                    <div class="academics-overview-style1-content__shape"
-                        style="background-image: url(assets/images/shape/academics-overview-style1-content-shape.png);">
-                    </div>
-                    <div class="row align-items-center">
-                        <div class="col-xl-5">
-                            <div class="about-style1-content">
-                                <div class="top">
-                                    <div class="text">
-                                        <h2 class="short-heading">About MKK</h2>
-                                        <p>MKK School was set up in 1953 by our founder chairman, Rai Bahadur Dr. Maharaj
-                                            Krishna Kapur. Presently it is run by Dr. Maharaj Krishna Kapur Educational
-                                            Charitable Trust and M anagement Society. It is a co-educational day school
-                                            having a strength of twenty nine hundred students. The school has an
-                                            excellent infrastructure which includes a huge sports complex, laboratories
-                                            (Physics, Chemistry, Bio, I.T., Maths, Artificial Intelligence & Robotics,
-                                            Tinkering) canteen, gym, library, auditorium, multipurpose hall, activity
-                                            block (music, art, dance) smart classrooms, charitable school dispensary and
-                                            artificial limb centre.</p>
+                        <div class="academics-overview-style1-content__shape"
+                            style="background-image: url(assets/images/shape/academics-overview-style1-content-shape.png);">
+                        </div>
+                        <div class="row align-items-center">
+                            <div class="col-xl-5">
+                                <div class="about-style1-content">
+                                    <div class="top">
+                                        <div class="text">
+                                            <h2 class="short-heading">About MKK</h2>
+                                            <p>MKK School was set up in 1953 by our founder chairman, Rai Bahadur Dr.
+                                                Maharaj
+                                                Krishna Kapur. Presently it is run by Dr. Maharaj Krishna Kapur
+                                                Educational
+                                                Charitable Trust and M anagement Society. It is a co-educational day
+                                                school
+                                                having a strength of twenty nine hundred students. The school has an
+                                                excellent infrastructure which includes a huge sports complex,
+                                                laboratories
+                                                (Physics, Chemistry, Bio, I.T., Maths, Artificial Intelligence &
+                                                Robotics,
+                                                Tinkering) canteen, gym, library, auditorium, multipurpose hall,
+                                                activity
+                                                block (music, art, dance) smart classrooms, charitable school dispensary
+                                                and
+                                                artificial limb centre.</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="col-xl-7">
-                            <div class="about-style1-img-box">
-                                <div class="inner">
-                                    <img src="assets/images/about-us.jpg" alt="">
+                            <div class="col-xl-7">
+                                <div class="about-style1-img-box">
+                                    <div class="inner">
+                                        <img src="assets/images/about-us.jpg" alt="">
+                                    </div>
                                 </div>
                             </div>
+
                         </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="text-box1">
+                                    <ul>
+                                        <h3 class="short-heading">Our Mission</h3>
+                                        <li class="txt-white">Our mission is to provide a holistic education that
+                                            balances academic rigor
+                                            with the development of essential life skills. We aim to inspire a love for
+                                            learning, instill moral values, and cultivate a sense of social
+                                            responsibility in our students.</li>
 
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <div class="text-box1">
-                                <ul>
-                                    <h3 class="short-heading">Our Mission</h3>
-                                    <li class="txt-white">Our mission is to provide a holistic education that
-                                        balances academic rigor
-                                        with the development of essential life skills. We aim to inspire a love for
-                                        learning, instill moral values, and cultivate a sense of social
-                                        responsibility in our students.</li>
-
-                                    <h3 class="short-heading">Academic Excellence</h3>
-                                    <li class="txt-white">We pride ourselves on our strong academic programs,
-                                        designed to challenge
-                                        and engage students at every level. Our curriculum is comprehensive,
-                                        integrating the latest educational practices and technology to ensure
-                                        students achieve their full potential. Our dedicated and experienced faculty
-                                        are committed to delivering high-quality education and personalized support.
-                                    </li>
-                                    <h3 class="short-heading">Community and Values</h3>
-                                    <li class="txt-white">We are more than just a school; we are a community that
-                                        values respect,
-                                        integrity, and compassion. Our students are encouraged to be active
-                                        participants in their communities, promoting positive change and
-                                        understanding diverse perspectives.</li>
-                                </ul>
+                                        <h3 class="short-heading">Academic Excellence</h3>
+                                        <li class="txt-white">We pride ourselves on our strong academic programs,
+                                            designed to challenge
+                                            and engage students at every level. Our curriculum is comprehensive,
+                                            integrating the latest educational practices and technology to ensure
+                                            students achieve their full potential. Our dedicated and experienced faculty
+                                            are committed to delivering high-quality education and personalized support.
+                                        </li>
+                                        <h3 class="short-heading">Community and Values</h3>
+                                        <li class="txt-white">We are more than just a school; we are a community that
+                                            values respect,
+                                            integrity, and compassion. Our students are encouraged to be active
+                                            participants in their communities, promoting positive change and
+                                            understanding diverse perspectives.</li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 </section>
 
 <?php
-        include('_web/footer.php');
-        include('_web/script.php');
-        ?>
+include('_web/footer.php');
+include('_web/script.php');
+?>
