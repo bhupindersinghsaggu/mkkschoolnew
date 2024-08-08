5e8fc2e73a4e79fc78f3d815637dc81a32848da1
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-08-08 11:50:43 +0530

    news letter added for july month

---
+++ middle-wing.php
@@ -0,0 +1,121 @@
+<?php
+include('_web/header.php');
+include('_web/css.php');
+?>
+
+<section class="breadcrumb-area">
+    <div class="breadcrumb-area-bg" style="background-image: url(assets/images/breadcrumb/breadcrumb-1.jpg);">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="inner-content">
+                    <div class="title aos-init aos-animate" data-aos="fade-up" data-aos-easing="linear"
+                        data-aos-duration="1500">
+                        <h2>Middle Wing  <i class="baseline-event_busy"></i></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="marketplace-faq-area">
+    <div class="thm-pattern-style5" style="background-image: url(assets/images/pattern/thm-pattern-2.png);">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-8">
+                <div class="marketplace-faq-content">
+                    <!-- <div class="sec-title-style5 text-left">
+                        <h2>Session 2024-25</h2>
+                    </div> -->
+                    <div class="marketplace-faq-content__inner">
+                        <ul class="accordion-box">
+                            <li class="accordion block">
+                                <div class="acc-btn active">
+                                    <h3>
+                                        E-News Letters for Session 2024
+                                    </h3>
+                                    <div class="icon-outer">
+                                        <i class="icon-play"></i>
+                                    </div>
+                                </div>
+                                <div class="acc-content current" style="display: none;">
+                                    <div class="text">
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                July</a></h3>
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                June</a></h3>
+                                        <h3 class=" pd-10"><a href="assets/images/newsletter/2024/may.pdf"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                May</a></h3>
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                April</a></h3>
+                                    </div>
+                                </div>
+                            </li>
+                            <!-- <li class="accordion block">
+                                <div class="acc-btn">
+                                    <h3>
+                                        E-News Letters for Session 2023
+                                    </h3>
+                                    <div class="icon-outer">
+                                        <i class="icon-play"></i>
+                                    </div>
+                                </div>
+                                <div class="acc-content" style="display: none;">
+                                    <div class="text">
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                Mar</a></h3>
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                Feb</a></h3>
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                Jan</a></h3>
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                Dec</a></h3>
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                Nov</a></h3>
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                Oct</a></h3>
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                Spet</a></h3>
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                Aug</a></h3>
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                July</a></h3>
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                June</a></h3>
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                May</a></h3>
+                                        <h3 class=" pd-10"><a href="#"><i
+                                                    class="fa-solid fa-circle-down fa-beat pdr-5"></i>
+                                                April</a></h3>
+                                    </div>
+                                </div>
+                            </li> -->
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+        include('_web/footer.php');
+        include('_web/script.php');
+        ?>
