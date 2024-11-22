1566952505cca5aff8a735fa3a84f2cbb59c5d24
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-11-22 17:06:48 +0530

    website upload

---
+++ reports/teacher-observation-form.php
@@ -0,0 +1,43 @@
+<?php
+include('web/header.php');
+?>
+
+<div class="vertical-overlay"></div>
+        <div class="main-content">
+            <div class="page-content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col">
+                            <div class="h-100">
+                                <div class="row mb-3 pb-1">
+                                    <div class="row">
+                                        <div class="col-xl-12 col-md-6">
+                                            <div class="card card-animate">
+                                                <div class="card-body">
+                                                <div class="flex-grow-1">
+                                                <h4 class="fs-16 mb-1">Good Morning, MKK School</h4>
+                                            </div>
+                                                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdcIkHoZA9gDi9KQUzXRLx62FrMlO1EoE-N54AzwzrFKcGNNg/viewform?embedded=true" width="640" height="3470" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div> <!-- end .h-100-->
+                            </div> <!-- end col -->
+                        </div>
+                    </div>
+                    <!-- container-fluid -->
+                </div>
+                <!-- End Page-content -->
+                <?php 
+                include('web/footer.php');
+                ?>
+            </div>
+            <!-- end main content-->
+
+        </div>
+
+
+<?php
+include('web/footer.php');
+?>
