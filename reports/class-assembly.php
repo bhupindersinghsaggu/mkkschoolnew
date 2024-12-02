41497374b1fe42dccfe9f86441109f88804ef63c
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-12-02 11:41:22 +0530

    Update class-assembly.php

--- reports/class-assembly.php
+++ reports/class-assembly.php
@@ -7,9 +7,6 @@ include('web/header.php')
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="row">
             <div class="col-lg-6 col-md-4 order-1">
-                <iframe
-                    src="https://docs.google.com/forms/d/e/1FAIpQLSdr2UhmNrb5cMKOueHZKLz_YiEmQLBYcmeatl8dv-TNIuaf2A/viewform?embedded=true"
-                    width="100%" height="1256" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 
                     <div class="container" id="ff-compose"></div>
 <script async defer src="https://formfacade.com/include/102811184558856598345/form/1FAIpQLSfkW9hCbhpub21aH1y0LI2bv1Tf42h5C_1lAKjRv8L9cW6QIQ/bootstrap.js?div=ff-compose"></script>
