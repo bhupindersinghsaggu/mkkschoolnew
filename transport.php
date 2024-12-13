073df87a771241ee680a084a6eeb87f106758f44
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-12-13 11:30:59 +0530

    Create transport.php

---
+++ transport.php
@@ -0,0 +1,115 @@
+<?php
+include('_web/header.php');
+include('_web/css.php');
+?>
+
+<section class="breadcrumb-style7-area">
+    <div class="breadcrumb-style7-area-bg" style="background-image: url(assets/images/header/our_objective.jpg);">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="inner-content">
+                    <div class="title aos-init aos-animate pd-100" data-aos="fade-up" data-aos-easing="linear"
+                        data-aos-duration="1500">
+                        <h2>About Us</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="cooking-course-choose-area">
+    <div class="container">
+        <div class="sec-title-style8">
+            <h2>Transport <span> Rules</span></h2>
+        </div>
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="cooking-course-choose-text-box">
+                <ul class="mt-50">
+                                <li>
+                                    <div class="text">
+                                        <p> 1. Coming to school on Motorcycle/Scooters is totally banned. Parking of Motorcycle/Scooters inside the school premises is not allowed. Due to the complaint of HUDA Residents’ Welfare Association, Panipat, Traffic Police has restricted the parking of vehicles around the school premises. If any vehicle is parked there, it will be confiscated. So the students are advised not to come to school on any Motor Cycle/Activa/Car etc. Parents are requested to send students either in school transport or personally drop them.
+                                        </p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="text">
+                                        <p>2.  At the beginning of the session, parents will have to give an undertaking that their wards will not bring any automobile to the school. If they bring any, they must have proper licence, helmet and other papers required.</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="text">
+                                        <p>3.  The bus facilities are extended as a privilege and can't be claimed as a right. </p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="text">
+                                        <p> 4.  The bus facility may be withdrawn at any time due to unavoidable circumstances. </p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="text">
+                                        <p> 5. Transport facility is available for the whole session. No withdrawals are allowed in between.</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="text">
+                                        <p> 6.
+ Transport facility is withdrawn in case any student is found damaging/indulging in indiscipline in the bus.
+</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="text">
+                                        <p> 7.  Students can apply for the transport facility only at the beginning of the session. Application for availing or withdrawing transport facility will not be entertained after that.</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="text">
+                                        <p> 8.  Any request to change the bus-stop/route must be submitted to the Principal at least one week prior to the change that is to be made. </p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="text">
+                                        <p> 9. The buses will not wait for the late comers.</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="text">
+                                        <p> 10. No student should come near the entry door of the bus until it stop.</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="text">
+                                        <p> 11.  The student should stay away from the main road until the bus arrives. </p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="text">
+                                        <p> 12.  The drivers are authorised to stop buses at the designated stop only. The list of stop is subject to change from time to time. </p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="text">
+                                        <p> 13.  The following acts are considered serious violation :</p>
+                                    </div>
+                                    <ul class="mt-10">
+                                        <li class="list-item-sub">(a) Attempt to distract driver's attention. </li>
+                                        <li class="list-item-sub">(b) Shrieking and shouting in the bus. </li>
+                                        <li class="list-item-sub">(c) Damaging bus property.</li>
+                                        <li class="list-item-sub">(d) Discourtesy to other children and passersby.</li>
+                                    </ul>
+                                </li>
+                               
+                            </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+include('_web/footer.php');
+include('_web/script.php');
+?>
