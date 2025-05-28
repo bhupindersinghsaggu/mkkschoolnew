ed3852476477b7be1b0c3734917f13878cdb687b
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-05-28 11:56:04 +0530

    Update project-details-RIDS.php

--- reports/project-details-RIDS.php
+++ reports/project-details-RIDS.php
@@ -8,8 +8,13 @@ include('web/functions.php');
     <div class="container-xxl flex-grow-1 container-p-y">
         <h4>Project Details RIDS</h4>
         <div class="row g-6">
-            <div class="col-md-12">
+            <div class="col-md-6">
                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdA6Lvq6LasqAgSJOkg4MmmbDSdc3PiRqY14gHWABNzMd_itw/viewform?embedded=true" width="640" height="2000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
+            <div class="col-md-6">
+              <div class="button-group mt-3">
+                                    <a href="https://docs.google.com/spreadsheets/d/1tmenTFJZGO3YV3V-ql8fu35M976hgj9_ErdrX7UtSso/edit?resourcekey=&gid=1515377441#gid=1515377441" class="btn btn-sm btn-dark mb-1">View Response</a>
+                                </div>
             </div>
         </div>
 
