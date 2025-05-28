998b418f0b29565b742a554a7732c59895011f80
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-05-28 11:48:20 +0530

    Create project-details-RIDS.php

---
+++ reports/project-details-RIDS.php
@@ -0,0 +1,20 @@
+<?php
+include('web/header.php');
+include('web/functions.php');
+?>
+<!-- Content wrapper -->
+<div class="content-wrapper">
+    <!-- Content -->
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <h5>CBSE Result Class X & XII</h5>
+        <div class="row g-6">
+            <div class="col-md-12">
+               
+            </div>
+        </div>
+
+    </div>
+    <!-- / Content -->
+    <?php
+    include('web/footer.php')
+    ?>
