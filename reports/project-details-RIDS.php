402c9f7b19dd032487f50e5f0aca17139f743148
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-05-28 17:21:46 +0530

    Update project-details-RIDS.php

--- reports/project-details-RIDS.php
+++ reports/project-details-RIDS.php
@@ -6,7 +6,7 @@ include('web/functions.php');
 <div class="content-wrapper">
     <!-- Content -->
     <div class="container-xxl flex-grow-1 container-p-y">
-        <h4>Project Details RIDS</h4>
+        <h4>Fill the Form </h4>
         <div class="row g-6">
             <div class="col-md-6">
                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdA6Lvq6LasqAgSJOkg4MmmbDSdc3PiRqY14gHWABNzMd_itw/viewform?embedded=true" width="640" height="2000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
