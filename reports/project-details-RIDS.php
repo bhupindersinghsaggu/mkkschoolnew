f5a1f8cb9062df72d3c458f29d991b24dad699e3
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-05-28 11:50:13 +0530

    Update project-details-RIDS.php

--- reports/project-details-RIDS.php
+++ reports/project-details-RIDS.php
@@ -6,10 +6,10 @@ include('web/functions.php');
 <div class="content-wrapper">
     <!-- Content -->
     <div class="container-xxl flex-grow-1 container-p-y">
-        <h5>CBSE Result Class X & XII</h5>
+        <h4>Project Details RIDS</h4>
         <div class="row g-6">
             <div class="col-md-12">
-               
+               <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdA6Lvq6LasqAgSJOkg4MmmbDSdc3PiRqY14gHWABNzMd_itw/viewform?embedded=true" width="640" height="1524" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             </div>
         </div>
 
