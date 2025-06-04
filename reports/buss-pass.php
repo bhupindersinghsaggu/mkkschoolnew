fb5e63e365d90f3b1c6ba3208a34bf16975e67fe
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-06-04 12:12:55 +0530

    add buss pass

--- reports/buss-pass.php
+++ reports/buss-pass.php
@@ -6,14 +6,14 @@ include('web/functions.php');
 <div class="content-wrapper">
     <!-- Content -->
     <div class="container-xxl flex-grow-1 container-p-y">
-        <h4>Fill the Form </h4>
+        <h4>Buss Pass For Transport Users </h4>
         <div class="row g-6">
             <div class="col-md-6">
                 <div class="button-group mt-3">
-                    <a href="https://docs.google.com/spreadsheets/d/1tmenTFJZGO3YV3V-ql8fu35M976hgj9_ErdrX7UtSso/edit?resourcekey=&gid=1515377441#gid=1515377441" class="btn btn-xl btn-primary mb-1">Download PDF</a>
+                    <a href="assets/img/docs/bus-pass.pdf" class="btn btn-xl btn-primary mb-1">Download PDF</a>
                 </div>
                         <div class="button-group mt-3">
-                    <a href="https://docs.google.com/spreadsheets/d/1tmenTFJZGO3YV3V-ql8fu35M976hgj9_ErdrX7UtSso/edit?resourcekey=&gid=1515377441#gid=1515377441" class="btn btn-xl btn-primary mb-1">Download Word</a>
+                    <a href="assets/img/docs/bus-pass.docx" class="btn btn-xl btn-danger mb-1">Download Word</a>
                 </div>
             </div>
         </div>
