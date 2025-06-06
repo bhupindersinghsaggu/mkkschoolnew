fcd04fa84e6e6cd16a497ace1967e57106f53ba3
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-06-06 16:06:57 +0530

    Update udise-reports.php

--- reports/udise-reports.php
+++ reports/udise-reports.php
@@ -12,6 +12,9 @@ include('web/functions.php');
                 <iframe src="https://forms.gle/weToHdC9UiX4ztd67\" width="640" height="400" frameborder="0" marginheight="0" marginwidth="0"></iframe>
             </div>
             <div class="col-md-6">
+
+            <h3>For Previos Session Report Card Use <b> common id for ERP</b> </h3>
+
                 <div class="button-group mt-3">
                     <a href="https://docs.google.com/spreadsheets/d/1Dwa1BP003U7HOvNhKZ1CehdTeUkqo99QeazwkpbI6Ig/edit?usp=sharing" class="btn btn-xl btn-primary mb-1">View Response</a>
                 </div>
