4455154bcaed873528995315b410816474d465b1
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-06-17 21:28:54 -0700

    Create index.php

---
+++ schoolofexcellence/admin/index.php
@@ -0,0 +1,88 @@
+<?php
+include('web/header.php');
+
+?>
+<!-- Content wrapper -->
+<div class="content-wrapper">
+    <!-- Content -->
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="row">
+            <div class="col-lg-6 col-md-4 order-1">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12 mb-6">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <div class="card-title d-flex align-items-start justify-content-between mb-4">
+                                    <div class="avatar flex-shrink-0">
+                                        <img src="assets/img/icons/unicons/chart-success.png" alt="chart success"
+                                            class="rounded">
+                                    </div>
+                                </div>
+                                <h3 class="mb-1"> Session: 2024-25</h3>
+                                <h5 class="card-title mb-3">CBSE Result Class X</h5>
+                                <div class="button-group">
+                                    <a href="cbse-reports.php" class="btn btn-sm btn-danger mb-1">View/Submit Result</a>
+                                </div>
+                                <div class="button-group mt-3">
+                                    <a href="https://drive.google.com/drive/u/0/folders/14J0woemDYABkRfk-AeAL0vLbVq6ktwvD" class="btn btn-sm btn-dark mb-1">All Compiled Reports</a>
+                                </div>
+                                <div class="mt-3">
+                                    <ul class="list-group">
+                                        <li class="mb-1 list-group-item"><i class="fas fa-paper-plane icon-color"></i> Student Enrolled: <strong> 269 </strong> </li>
+                                        <li class="mb-1 list-group-item"> <i class="fas fa-paper-plane icon-color"></i> Student Appeared: <strong> 268 </strong> </li>
+                                        <li class="mb-1 list-group-item"> <i class="fas fa-paper-plane icon-color"></i> Hightest Percentage: <strong> 99% </strong> </li>
+                                        <li class="mb-1 list-group-item"> <i class="fas fa-paper-plane icon-color"></i> Pass Percentage: <strong> 100% </strong> </li>
+                                        <li class="mb-1 list-group-item"> <i class="fas fa-paper-plane icon-color"></i> No of First Division: <strong> 239 </strong> </li>
+                                    </ul>
+                                </div>
+                                <!-- <h5 class="card-title mb-3 mt-3">All Reports</h5> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12  mb-6">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <div class="card-title d-flex align-items-start justify-content-between mb-4">
+                                    <div class="avatar flex-shrink-0">
+                                        <img src="assets/img/icons/unicons/wallet-info.png" alt="wallet info"
+                                            class="rounded">
+                                    </div>
+                                </div>
+                                <h3 class="mb-1"> Session: 2024-25</h3>
+                                <h5 class="card-title mb-3">CBSE Result Class XII</h5>
+                                <div class="button-group mt-3">
+                                    <a href="cbse-reports.php" class="btn btn-sm btn-primary mb-1">View/Submit Result</a>
+                                </div>
+                                <div class="button-group mt-3">
+                                    <a href="https://drive.google.com/drive/u/0/folders/1IwZv2kuKRAlG-qDuKq-oYM40rQ8Bhp4d" class="btn btn-sm btn-dark mb-1">All Compiled Reports</a>
+                                </div>
+                                <div class="mt-3">
+                                    <ul class="list-group">
+                                        <li class="mb-1 list-group-item"><i class="fas fa-paper-plane icon-color"></i> Student Enrolled: <strong> 262 </strong> </li>
+                                        <li class="mb-1 list-group-item"> <i class="fas fa-paper-plane icon-color"></i> Student Appeared: <strong> 261 </strong> </li>
+                                        <li class="mb-1 list-group-item"> <i class="fas fa-paper-plane icon-color"></i> Hightest Percentage: <strong> 98.2% </strong> </li>
+                                        <li class="mb-1 list-group-item"> <i class="fas fa-paper-plane icon-color"></i> Pass Percentage: <strong> 100% </strong> </li>
+                                        <li class="mb-1 list-group-item"> <i class="fas fa-paper-plane icon-color"></i> No of First Division: <strong> 256 </strong> </li>
+                                    </ul>
+                                </div>
+                                <!-- <h5 class="card-title mb-3 mt-3">All Reports</h5> -->
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- <div class="col-lg-6 col-md-12  mb-6">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <iframe
+                                    src="https://curtina.in/_EazySchool/forms/frmRP_Sp_Admission_ClassWise_JQ.aspx"></iframe>
+                            </div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+        <!-- / Content -->
+        <?php
+        include('web/footer.php')
+        ?>
