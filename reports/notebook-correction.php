a0a38fa0cf4499b8e7e612a78c4672189e33fc05
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-07-10 13:46:21 +0530

    dfgdf

--- reports/notebook-correction.php
+++ reports/notebook-correction.php
@@ -7,9 +7,13 @@ include('web/header.php')
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="row">
             <div class="col-lg-6 col-md-4 order-1">
-
                 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdtkFRjibah7kHwxVzKGBlFaG2ApR4yzn6PQX2GCQWE-2-llA/viewform?embedded=true" width="100%" height="3470" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                 <a href="index.php" class="btn btn-danger btn-buy-now">Back</a>
+            </div>
+            <div class="col-lg-6 col-md-4 order-1">
+                <div class="button-group">
+                    <a href="https://docs.google.com/spreadsheets/d/1Z6RqSnZbM2B8sxDkOGF4RcitxUDEeAe2bi4609jTOfw/edit?resourcekey=&gid=1894534079#gid=1894534079" class="btn btn-lg btn-danger mb-1">View Record</a>
+                </div>
             </div>
         </div>
     </div>
