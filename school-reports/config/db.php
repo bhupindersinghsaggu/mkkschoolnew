8c3ef831ae886755a79820aa8a35e04410fca59f
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-07-25 13:23:50 +0530

    Update db.php

--- school-reports/config/db.php
+++ school-reports/config/db.php
@@ -1,20 +1,20 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "mkk-school");
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-?>
 
-
-<?php
-// $host = "localhost";
-// $user = "u241734313_designspocket";
-// $pass = "A[sI/g4>a";
-// $dbname = "u241734313_designspocket";
-// $conn = new mysqli($host, $user, $pass, $dbname);
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
+// $conn = mysqli_connect("localhost", "root", "", "mkk-school");
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
 // }
+
+
+
+<?php
+$host = "localhost";
+$user = "u315669183_school_reports";
+$pass = "@Av4652B]m";
+$dbname = "u315669183_school_reports";
+$conn = new mysqli($host, $user, $pass, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
 
 
