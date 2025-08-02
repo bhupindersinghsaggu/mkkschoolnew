1fc1cd7c91b4866c32034f965a54a47b715c5825
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-02 14:35:13 +0530

    Update list.php

--- school-reports/notebook/list.php
+++ school-reports/notebook/list.php
@@ -131,8 +131,8 @@ $result = mysqli_query($conn, $query);
                                 <!-- <td><?= $row['students_reviewed'] ?></td> -->
                                 <td><?= $row['overall_rating'] ?></td>
                                 <td>
-                                    <a href="edit.php?id=<?= $row['id'] ?>"><i class="fas fa-edit"></i> Edit</a>
-                                    <a href="delete.php?id=<?= $row['id'] ?>" class="btn btn-danger btn-sm" onclick="return confirm('Delete this?')">Delete</a>
+                                    <a href="edit.php?id=<?= $row['id'] ?>"><i class="fas fa-edit"></i></a>
+                                    <a href="delete.php?id=<?= $row['id'] ?>" onclick="return confirm('Delete this?')"><i class="fa-solid fa-trash-can"></i></a>
                                     <a href="print_single.php?id=<?= $row['id'] ?>" class="btn btn-info btn-sm" target="_blank">Print</a>
                                 </td>
                             </tr>
