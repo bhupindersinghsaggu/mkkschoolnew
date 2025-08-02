ce6b38e94dfc2dd2bec028d403e2c0975a02fc01
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-02 15:19:59 +0530

    Update delete.php

--- school-reports/notebook/delete.php
+++ school-reports/notebook/delete.php
@@ -1,11 +1,11 @@
 <?php
-session_start();
-include '../config/db.php';
+// session_start();
+// include '../config/db.php';
 
-if (!isset($_SESSION['loggedin'])) {
-    header("Location: login.php");
-    exit;
-}
+// if (!isset($_SESSION['loggedin'])) {
+//     header("Location: login.php");
+//     exit;
+// }
 
 $id = $_GET['id'];
 mysqli_query($conn, "DELETE FROM records WHERE id=$id");
