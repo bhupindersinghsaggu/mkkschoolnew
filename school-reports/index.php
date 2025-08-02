1740de8cbf0441e6bd1fd7f21dbe347221b48e31
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-02 12:56:46 +0530

    Update index.php

--- school-reports/index.php
+++ school-reports/index.php
@@ -36,7 +36,12 @@
 								<circle cx="12" cy="12" r="10"></circle>
 								<line x1="12" y1="8" x2="12" y2="16"></line>
 								<line x1="8" y1="12" x2="16" y2="12"></line>
-							</svg>Add Teacher</a>
+							</svg>Add </a>
+							<a href="../school-reports/teacher/add.php" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#add-purchase"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle me-1">
+								<circle cx="12" cy="12" r="10"></circle>
+								<line x1="12" y1="8" x2="12" y2="16"></line>
+								<line x1="8" y1="12" x2="16" y2="12"></line>
+							</svg>View </a>
 					</div>
 				</div>
 
@@ -61,7 +66,12 @@
 								<circle cx="12" cy="12" r="10"></circle>
 								<line x1="12" y1="8" x2="12" y2="16"></line>
 								<line x1="8" y1="12" x2="16" y2="12"></line>
-							</svg>Add Record</a>
+							</svg>Add </a>
+							<a href="../school-reports/notebook/list.php" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#add-purchase"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle me-1">
+								<circle cx="12" cy="12" r="10"></circle>
+								<line x1="12" y1="8" x2="12" y2="16"></line>
+								<line x1="8" y1="12" x2="16" y2="12"></line>
+							</svg>View </a>
 					</div>
 				</div>
 			</div>
