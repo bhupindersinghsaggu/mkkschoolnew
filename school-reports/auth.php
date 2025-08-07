408547a4f4d0382ed3cac34472b825b36f905f8d
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-07 23:06:47 +0530

    Update auth.php

--- school-reports/auth.php
+++ school-reports/auth.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+if (!isset($_SESSION['loggedin']) || $_SESSION['role'] !== 'teacher') {
     header("Location: login.php");
     exit;
 }
+
 ?>
