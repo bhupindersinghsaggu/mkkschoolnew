75cd75c4528724822882b52d9e1930bb8f4fc34b
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-09 23:53:53 +0530

    Update database.php

--- test/config/database.php
+++ test/config/database.php
@@ -11,10 +11,10 @@
 ?>
 -->
 <?php
-$host = 'localhost';
-$username = 'u315669183_school_erp';
+$host = 'mkkschool.com';
+$username = 'u315669183_school_reports';
 $password = 'V1N251[0$r';
-$dbname = 'u315669183_school_erp';
+$dbname = 'u315669183_school_reports';
 $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
