7a25db3488803ad2027d809e7e0c845326689fc4
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-09 23:30:00 +0530

    Update database.php

--- test/config/database.php
+++ test/config/database.php
@@ -1,3 +1,15 @@
+<!-- <?php 
+// $host = 'localhost';
+// $dbname = 'school_reports';
+// $username = 'root';
+// $password = '';
+// $conn = new mysqli($host, $username, $password, $dbname);
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+?>
+-->
 <?php
 $host = 'localhost';
 $dbname = 'u315669183_school_reports';
