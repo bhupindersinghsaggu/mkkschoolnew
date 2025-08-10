7d083efb3e37157f29b9eb1600f23778a5a50fb7
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-10 13:59:56 +0530

    Update database.php

--- school-reports/config/database.php
+++ school-reports/config/database.php
@@ -1,29 +1,14 @@
-<!-- <?php 
-// $host = 'localhost';
-// $dbname = 'school_reports';
-// $username = 'root';
-// $password = '';
-// $conn = new mysqli($host, $username, $password, $dbname);
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
 
-?>
--->
-<?php
-$host = 'localhost';
-$username = 'u315669183_school_reports';
-$password = 'E*QPe#Y3';
-$dbname = 'u315669183_school_reports';
-$conn = new mysqli($host, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-// try {
-//     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//     die("Database connection failed: " . $e->getMessage());
-// }
+
+ <?php 
+ $host = 'localhost';
+ $username = 'u315669183_school_reports';
+ $password = 'E*QPe#Y3';
+ $dbname = 'u315669183_school_reports';
+ $conn = new mysqli($host, $username, $password, $dbname);
+ if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+ }
+
 ?>
 
