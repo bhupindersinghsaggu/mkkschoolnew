e7d3da92d33cbf135dfb446bfa87791592e76cca
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-11 09:40:05 +0530

    Update head-nav.php

--- school-reports/includes/head-nav.php
+++ school-reports/includes/head-nav.php
@@ -4,12 +4,12 @@
             <a href="../index.php" class="logo logo-normal">
                 <h3>M.K.K. School</h3>
             </a>
-            <a href="../index.php" class="logo logo-white">
+            <!-- <a href="../index.php" class="logo logo-white">
                 <h3>M.K.K. School</h3>
             </a>
             <a href="../index.php" class="logo-small">
                 <h3>M.K.K. School</h3>
-            </a>
+            </a> -->
         </div>
         <a id="mobile_btn" class="mobile_btn" href="#sidebar">
             <span class="bar-icon">
