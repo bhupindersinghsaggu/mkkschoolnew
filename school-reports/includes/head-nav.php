bb72fbd8becef5494f347734beeb4d2375b387c9
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-11 09:38:38 +0530

    Update head-nav.php

--- school-reports/includes/head-nav.php
+++ school-reports/includes/head-nav.php
@@ -1,21 +1,16 @@
 <div class="header">
     <div class="main-header">
-        <!-- Logo -->
         <div class="header-left active">
-            <a href="../index.php" class="logo logo-normal">
+            <!-- <a href="../index.php" class="logo logo-normal"> -->
                 <h3>M.K.K. School</h3>
-                <!-- <img src="../assets/img/logo.svg" alt="Img"> -->
-            </a>
-            <a href="../index.php" class="logo logo-white">
+            <!-- </a> -->
+            <!-- <a href="../index.php" class="logo logo-white"> -->
                 <h3>M.K.K. School</h3>
-                <!-- <img src="../assets/img/logo-white.svg" alt="Img"> -->
-            </a>
-            <a href="../index.php" class="logo-small">
+            <!-- </a> -->
+            <!-- <a href="../index.php" class="logo-small"> -->
                 <h3>M.K.K. School</h3>
-                <!-- <img src="../assets/img/logo-small.png" alt="Img"> -->
-            </a>
+            <!-- </a> -->
         </div>
-        <!-- /Logo -->
         <a id="mobile_btn" class="mobile_btn" href="#sidebar">
             <span class="bar-icon">
                 <span></span>
@@ -23,7 +18,6 @@
                 <span></span>
             </span>
         </a>
-        <!-- Header Menu -->
         <div class="logout d-flex ">
             <?php if (isset($_SESSION['user_id'])): ?>
                 <a href="../auth/logout.php" class="btn btn-secondary me-2 logout-link">
