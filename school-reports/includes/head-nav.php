7b1909cb855adbf0b8f526786d90394c66b384ea
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-11 10:19:50 +0530

    Update head-nav.php

--- school-reports/includes/head-nav.php
+++ school-reports/includes/head-nav.php
@@ -3,7 +3,7 @@
         <div class="header-left active">
             <a href="../index.php" class="logo logo-normal">
                 <h3>M.K.K. School</h3>
-            </a> 
+            </a>
             <a href="../index.php" class="logo logo-white">
                 <h3>M.K.K. School</h3>
             </a>
@@ -18,14 +18,14 @@
                 <span></span>
             </span>
         </a>
-        <div class="logout d-flex d-md-flex">
+        <div class="logout d-flex d-none d-md-flex">
             <?php if (isset($_SESSION['user_id'])): ?>
                 <a href="../auth/logout.php" class="btn btn-secondary me-2 logout-link">
                     <i class="fas fa-sign-out-alt"></i> Logout
-                    <!-- (<?php echo htmlspecialchars($username); ?>) -->
                 </a>
             <?php endif; ?>
         </div>
-       
+
+
     </div>
 </div>
