0556e33b449b06ffd49a7c7bd7bee59fc8b8f145
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-11 15:17:21 +0530

    Update dashboard.php

--- school-reports/teacher/dashboard.php
+++ school-reports/teacher/dashboard.php
@@ -31,8 +31,8 @@ if ($stmt) {
 }
 
 // Now include header after all PHP processing is done
-require_once './teacher/header.php';
-require_once './teacher/side-bar.php';
+require_once '../teacher/header.php';
+require_once '../teacher/side-bar.php';
 ?>
 
 <div class="page-wrapper">
