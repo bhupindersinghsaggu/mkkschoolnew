dfb6c5c832c59d17031dfb51ae8bb0ea4a0f37ac
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-11 09:46:38 +0530

    Update side-bar.php

--- school-reports/includes/side-bar.php
+++ school-reports/includes/side-bar.php
@@ -1,6 +1,6 @@
  <div class="sidebar" id="sidebar">
      <div class="sidebar-logo active">
-         <!-- <a href="../super_admin/dashboard.php" class="logo logo-normal">
+         <a href="../super_admin/dashboard.php" class="logo logo-normal">
               <h3>M.K.K. School</h3>
          </a>
          <a href="../super_admin/dashboard.php" class="logo logo-white">
@@ -11,7 +11,7 @@
          </a>
          <a id="toggle_btn" href="javascript:void(0);">
              <i data-feather="chevrons-left" class="feather-16"></i>
-         </a> -->
+         </a>
      </div>
      <div class="sidebar-inner slimscroll">
          <div id="sidebar-menu" class="sidebar-menu">
