76641ecfdaa381fe5fbb437b31069baba6206028
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-11 11:19:44 +0530

    Update login.php

--- school-reports/auth/login.php
+++ school-reports/auth/login.php
@@ -42,39 +42,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>M.K.K. School - Login</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <script src="../assets/js/theme-script.js" type="baf560cacd13bfb28c23b3e3-text/javascript"></script>
-    <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="../assets/img/favicon.png">
-    <!-- Apple Touch Icon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/apple-touch-icon.png">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-    <!-- Datetimepicker CSS -->
-    <link rel="stylesheet" href="../assets/css/bootstrap-datetimepicker.min.css">
-    <!-- animation CSS -->
-    <link rel="stylesheet" href="../assets/css/animate.css">
-    <!-- Select2 CSS -->
-    <link rel="stylesheet" href="../assets/plugins/select2/css/select2.min.css">
-    <!-- Daterangepikcer CSS -->
-    <link rel="stylesheet" href="../assets/plugins/daterangepicker/daterangepicker.css">
-    <!-- Tabler Icon CSS -->
-    <link rel="stylesheet" href="../assets/plugins/tabler-icons/tabler-icons.min.css">
-    <!-- Fontawesome CSS -->
-    <link rel="stylesheet" href="../assets/plugins/fontawesome/css/fontawesome.min.css">
-    <link rel="stylesheet" href="../assets/plugins/fontawesome/css/all.min.css">
-    <!-- Color Picker Css -->
-    <link rel="stylesheet" href="../assets/plugins/%40simonwep/pickr/themes/nano.min.css">
-    <!-- Main CSS -->
-    <link rel="stylesheet" href="../assets/css/style.css">
-</head>
 
 <body>
     <div class="login-container">
