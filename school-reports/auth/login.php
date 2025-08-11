1c19502f56545a17904a5c327dc77da9d5b8ca03
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-11 11:13:44 +0530

    Update login.php

--- school-reports/auth/login.php
+++ school-reports/auth/login.php
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>M.K.K. School - Login</title>
     
     <!-- Favicon -->
