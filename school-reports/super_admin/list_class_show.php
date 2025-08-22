40d12fa5376d6356884bda36a87e1c06c33d9598
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-22 20:47:35 +0530

    Update list_class_show.php

--- school-reports/super_admin/list_class_show.php
+++ school-reports/super_admin/list_class_show.php
@@ -43,7 +43,7 @@ $result = mysqli_query($conn, $query);
         <div class="content">
             <div class="header-button d-flex justify-content-between align-items-center mb-3">
                 <h3>Class Show Records</h3>
-                <div>
+                <div class="d-flex">
                     <a href="add_class_show.php" class="btn btn-primary">Add </a>
                     <a href="./dashboard.php" class="btn btn-secondary">Dashboard</a>
                 </div>
