26ff0de3254c2ce7d8362e432beb713290932471
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-22 22:22:34 +0530

    Update edit_teacher.php

--- school-reports/super_admin/edit_teacher.php
+++ school-reports/super_admin/edit_teacher.php
@@ -163,7 +163,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             display: flex;
             flex-direction: column;
             justify-items: center;
-            align-items: center;
+            align-items: start;
         }
 
         .profile-pic-preview {
