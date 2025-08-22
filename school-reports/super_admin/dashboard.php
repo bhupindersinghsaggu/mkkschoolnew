479c5a134f7634e438919e887dede1d62a35c5d7
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-22 22:32:18 +0530

    Update dashboard.php

--- school-reports/super_admin/dashboard.php
+++ school-reports/super_admin/dashboard.php
@@ -19,7 +19,7 @@ require_once '../includes/header.php';
         </div>
         <div class="row">
             <div class="col-xl-3 col-sm-6 col-12 d-flex">
-                <div class="card bg-primary sale-widget flex-fill">
+                <div class="card bg-success sale-widget flex-fill">
                     <div class="card-body d-flex align-items-center">
                         <span class="sale-icon bg-white text-primary">
                             <i class="ti ti-file-text fs-24"></i>
