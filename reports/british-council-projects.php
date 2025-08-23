46d9ffc88fd00442043ce721a96e515081d5752f
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-23 14:10:08 +0530

    project added

---
+++ reports/british-council-projects.php
@@ -0,0 +1,46 @@
+<?php
+include('web/header.php');
+include('web/functions.php');
+?>
+<!-- Content wrapper -->
+<div class="content-wrapper">
+    <!-- Content -->
+    <div class="container-xxl flex-grow-1 container-p-y">
+
+
+        <h5>British Council Projects</h5>
+        <div class="row g-6">
+            <div class="col-md-12">
+                <div class="accordion mt-4" id="accordionExample">
+                    <div class="accordion-item ">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button type="button" class="accordion-button btn-info" data-bs-toggle="collapse" data-bs-target="#accordionOne" aria-expanded="false" aria-controls="accordionOne">Class X Results Session 2024-25</button>
+                        </h2>
+                        <div id="accordionOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <div class="row mb-12 g-6">
+                                    <div class="col-md-6 col-lg-4">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h5 class="card-title">green thumb farming </h5>
+                                                <div class="demo-inline-spacing mt-4">
+                                                    <div class="list-group">
+                                                        <a href="https://www.canva.com/design/DAGwuaESUoc/7HjpROPAsJ_-rieNOWt9nQ/edit?utm_content=DAGwuaESUoc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" class="btn btn-dark">Video Link</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- / Content -->
+    <?php
+    include('web/footer.php')
+    ?>
