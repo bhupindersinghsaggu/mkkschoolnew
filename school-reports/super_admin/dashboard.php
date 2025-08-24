4f47e2240978f05a6e7f8fbd76068562bef4a75a
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-24 15:46:59 +0530

    Update dashboard.php

--- school-reports/super_admin/dashboard.php
+++ school-reports/super_admin/dashboard.php
@@ -17,84 +17,13 @@ require_once '../includes/header.php';
                 <input type="text" class="form-control date-range bookingrange" placeholder="Search Product">
             </div>
         </div>
-
-        <div class="alert bg-orange-transparent alert-dismissible fade show mb-4">
+        <!-- <div class="alert bg-orange-transparent alert-dismissible fade show mb-4">
             <div>
                 <span><i class="ti ti-info-circle fs-14 text-orange me-2"></i>Your Product </span> <span class="text-orange fw-semibold"> Apple Iphone 15 is running Low, </span> already below 5 Pcs., <a href="javascript:void(0);" class="link-orange text-decoration-underline fw-semibold" data-bs-toggle="modal" data-bs-target="#add-stock">Add Stock</a>
             </div>
             <button type="button" class="btn-close text-gray-9 fs-14" data-bs-dismiss="alert" aria-label="Close"><i class="ti ti-x"></i></button>
-        </div>
-
+        </div> -->
         <div class="row">
-            <div class="col-xl-3 col-sm-6 col-12 d-flex">
-                <div class="card bg-primary sale-widget flex-fill">
-                    <div class="card-body d-flex align-items-center">
-                        <span class="sale-icon bg-white text-primary">
-                            <i class="ti ti-file-text fs-24"></i>
-                        </span>
-                        <div class="ms-2">
-                            <p class="text-white mb-1">Total Sales</p>
-                            <div class="d-inline-flex align-items-center flex-wrap gap-2">
-                                <h4 class="text-white">$48,988,078</h4>
-                                <span class="badge badge-soft-primary"><i class="ti ti-arrow-up me-1"></i>+22%</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 col-12 d-flex">
-                <div class="card bg-secondary sale-widget flex-fill">
-                    <div class="card-body d-flex align-items-center">
-                        <span class="sale-icon bg-white text-secondary">
-                            <i class="ti ti-repeat fs-24"></i>
-                        </span>
-                        <div class="ms-2">
-                            <p class="text-white mb-1">Total Sales Return</p>
-                            <div class="d-inline-flex align-items-center flex-wrap gap-2">
-                                <h4 class="text-white">$16,478,145</h4>
-                                <span class="badge badge-soft-danger"><i class="ti ti-arrow-down me-1"></i>-22%</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 col-12 d-flex">
-                <div class="card bg-teal sale-widget flex-fill">
-                    <div class="card-body d-flex align-items-center">
-                        <span class="sale-icon bg-white text-teal">
-                            <i class="ti ti-gift fs-24"></i>
-                        </span>
-                        <div class="ms-2">
-                            <p class="text-white mb-1">Total Purchase</p>
-                            <div class="d-inline-flex align-items-center flex-wrap gap-2">
-                                <h4 class="text-white">$24,145,789</h4>
-                                <span class="badge badge-soft-success"><i class="ti ti-arrow-up me-1"></i>+22%</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 col-12 d-flex">
-                <div class="card bg-info sale-widget flex-fill">
-                    <div class="card-body d-flex align-items-center">
-                        <span class="sale-icon bg-white text-info">
-                            <i class="ti ti-brand-pocket fs-24"></i>
-                        </span>
-                        <div class="ms-2">
-                            <p class="text-white mb-1">Total Purchase Return</p>
-                            <div class="d-inline-flex align-items-center flex-wrap gap-2">
-                                <h4 class="text-white">$18,458,747</h4>
-                                <span class="badge badge-soft-success"><i class="ti ti-arrow-up me-1"></i>+22%</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="row">
-
             <!-- Profit -->
             <div class="col-xl-3 col-sm-6 col-12 d-flex">
                 <div class="card revenue-widget flex-fill">
