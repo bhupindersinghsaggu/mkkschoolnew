6a3d547423ea7acd5befd6b01309826154b3cfe4
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-24 17:03:12 +0530

    Update dashboard.php

--- school-reports/super_admin/dashboard.php
+++ school-reports/super_admin/dashboard.php
@@ -9,10 +9,12 @@ require_once '../includes/function.php';
 <div class="page-wrapper">
     <div class="content">
         <div class="d-flex align-items-center justify-content-between flex-wrap gap-3 mb-2">
-            <div class="mb-3 d-flex align-items-center">
-                <h1 class="mb-1">Welcome, Admin</h1>
-                <h3 class="px-3"><?php echo getGreetingWithEmoji(); ?>!</h3>
-                <!-- <p class="fw-medium">You have <span class="text-primary fw-bold">200+</span> Orders, Today</p> -->
+            <div class="card-header">
+                <div class="mb-3 d-flex align-items-center">
+                    <h1 class="mb-1">Welcome, Admin</h1>
+                    <h3 class="px-3"><?php echo getGreetingWithEmoji(); ?>!</h3>
+                    <!-- <p class="fw-medium">You have <span class="text-primary fw-bold">200+</span> Orders, Today</p> -->
+                </div>
             </div>
             <div class="input-icon-start position-relative mb-3">
                 <span class="input-icon-addon fs-16 text-gray-9">
