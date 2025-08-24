6dfaee01c732e80cba74ff762bd7a786d36ca0e4
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-24 16:08:30 +0530

    Update dashboard.php

--- school-reports/super_admin/dashboard.php
+++ school-reports/super_admin/dashboard.php
@@ -1,8 +1,8 @@
 <?php
 require_once '../includes/auth_check.php';
 require_once '../includes/header.php';
-require_once '../includes/function.php';
 require_once '../config/database.php';
+require_once '../includes/function.php';
 
 ?>
 
