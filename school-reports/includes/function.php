723f2edd16406116882018f8618786fd4fd39054
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-24 16:37:56 +0530

    add date

--- school-reports/includes/function.php
+++ school-reports/includes/function.php
@@ -15,3 +15,38 @@ $count_result = mysqli_query($conn, $count_query);
 $count_data = mysqli_fetch_assoc($count_result);
 $notebook_count = $count_data['notebook_count'];
 ?>
+
+<?php
+
+/**Get current date in various formats*/
+
+function getCurrentDate($format = 'F j, Y') {
+    return date($format);
+}
+
+/** Get current year */
+function getCurrentYear() {
+    return date('Y');
+}
+
+/*** Get current date with time*/
+function getCurrentDateTime($format = 'F j, Y, g:i a') {
+    return date($format);
+}
+
+/*** Get current day and date*/
+function getCurrentDayDate() {
+    return date('l, F j, Y');
+}
+
+/*** Get current date in Indian format (DD-MM-YYYY)*/
+function getCurrentDateIndian() {
+    return date('d-m-Y');
+}
+
+/**
+ * Get current date in SQL format (YYYY-MM-DD)*/
+function getCurrentDateSQL() {
+    return date('Y-m-d');
+}
+?>
