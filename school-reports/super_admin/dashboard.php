6e10f5d30ec57d1bb96a4ac6053a2dc04b5900aa
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-24 17:21:54 +0530

    Update dashboard.php

--- school-reports/super_admin/dashboard.php
+++ school-reports/super_admin/dashboard.php
@@ -40,12 +40,10 @@ require_once '../includes/function.php';
                 <div class="card revenue-widget flex-fill">
                     <div class="card-body">
                         <div class="d-flex align-items-center justify-content-between mb-3 pb-3 border-bottom">
-                            <div class="d-flex align-items-center justify-content-between ">
-                                <p>Total Teacher</p>
-                                <span class="revenue-icon bg-cyan-transparent text-cyan">
-                                    <?php echo $total_teachers; ?>
-                                </span>
-                            </div>
+                            <p>Total Teacher</p>
+                            <span class="revenue-icon bg-cyan-transparent text-cyan">
+                                <?php echo $total_teachers; ?>
+                            </span>
                         </div>
                         <div class="d-flex align-items-center justify-content-between">
                             <a href="./add_teacher.php" class="btn btn-success btn-sm">Add New</a>
