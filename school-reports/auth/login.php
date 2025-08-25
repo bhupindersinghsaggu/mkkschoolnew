931de6fd97d69b0e2cd934e8f53d8b0f866e79d4
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-25 12:23:15 +0530

    Update login.php

--- school-reports/auth/login.php
+++ school-reports/auth/login.php
@@ -84,59 +84,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="account-content">
                 <div class="login-wrapper bg-img">
                     <div class="login-content authent-content">
-                        <div class="login-userset">
-                            <div class="login-logo logo-normal">
-                                <img src="../assets/img/logo-small.png" alt="img" style="max-width: 100px;">
-                            </div>
-                            <!-- <h3>Dr. M.K.K. Arya Model School</h3> -->
-                            <a href="login.php" class="login-logo logo-white">
-                                <img src="../assets/img/logo-white.svg" alt="Img">
-                            </a> <?php if (isset($error)): ?>
-                                <div class="alert alert-danger"><?php echo $error; ?></div>
-                            <?php endif; ?>
-                            <form method="POST">
-                                <div class="login-userheading">
-                                    <h3>Sign In</h3>
-                                </div>
-                                <div class="mb-3">
-                                    <label class="form-label">Username <span class="text-danger"> *</span></label>
-                                    <div class="input-group">
-                                        <input type="text" name="username" value="" class="form-control border-end-0">
-                                        <span class="input-group-text border-start-0">
-                                            <i class="ti ti-mail"></i>
-                                        </span>
-                                    </div>
-                                </div>
-                                <div class="mb-3">
-                                    <label class="form-label">Password <span class="text-danger"> *</span></label>
-                                    <div class="pass-group">
-                                        <input type="password" name="password" class="pass-input form-control">
-                                        <span class="ti toggle-password ti-eye-off text-gray-9"></span>
-                                    </div>
-                                </div>
-                                <div class="form-login authentication-check">
-                                    <div class="row">
-                                        <div class="col-12 d-flex align-items-center justify-content-between">
-                                            <div class="custom-control custom-checkbox">
-                                                <label class="checkboxs ps-4 mb-0 pb-0 line-height-1 fs-16 text-gray-6">
-                                                    <input type="checkbox" class="form-control">
-                                                    <span class="checkmarks"></span>Remember me
-                                                </label>
-                                            </div>
-                                            <div class="text-end">
-                                                <a class="text-orange fs-16 fw-medium" href="forgot-password.html">Forgot Password?</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-login">
-                                    <button type="submit" class="btn btn-primary w-100">Sign In</button>
-                                </div>
-                            </form>
-                            <div class="my-4 d-flex justify-content-center align-items-center copyright-text">
-                                <p>Developed By Bhupinder Singh (IT Department)</p>
-                            </div>
-                        </div>
+                        dfgfdg
                     </div>
                 </div>
             </div>
