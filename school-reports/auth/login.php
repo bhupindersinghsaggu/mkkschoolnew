d0892f472df0cb01229b616677ce2ac0d54de398
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-08-25 12:29:57 +0530

    Update login.php

--- school-reports/auth/login.php
+++ school-reports/auth/login.php
@@ -88,7 +88,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             --danger-color: #f72585;
         }
 
-        body {
+        /* body {
             background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
             background-size: 400% 400%;
             animation: gradient 15s ease infinite;
@@ -98,7 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             justify-content: center;
             align-items: center;
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        }
+        } */
 
         @keyframes gradient {
             0% {
