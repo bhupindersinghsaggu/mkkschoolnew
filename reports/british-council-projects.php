a19d4f0bcf7cb570664a2a41ab9607d514c0cf38
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-09-03 13:48:44 +0530

    Update british-council-projects.php

--- reports/british-council-projects.php
+++ reports/british-council-projects.php
@@ -22,11 +22,9 @@ include('web/functions.php');
                                     <div class="col-md-6 col-lg-4">
                                         <div class="card">
                                             <div class="card-body">
-                                                <!-- <h5 class="card-title">green thumb farming </h5> -->
-                                                <img src="../reports/assets/img/green.jpg" width="300px">
                                                 <div class="demo-inline-spacing mt-4">
                                                     <div class="list-group">
-                                                        <a href="https://www.canva.com/design/DAGwuaESUoc/7HjpROPAsJ_-rieNOWt9nQ/edit?utm_content=DAGwuaESUoc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" class="btn btn-dark">Video Link</a>
+                                                      <a href="https://www.canva.com/design/DAGwuaESUoc/7HjpROPAsJ_-rieNOWt9nQ/edit?utm_content=DAGwuaESUoc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" class="btn btn-dark"> View Project </a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -38,19 +36,18 @@ include('web/functions.php');
                     </div>
                      <div class="accordion-item ">
                         <h2 class="accordion-header" id="headingOne">
-                            <button type="button" class="accordion-button btn-info" data-bs-toggle="collapse" data-bs-target="#accordionOne" aria-expanded="false" aria-controls="accordionOne">The Cry of the Ocean </button>
+                            <button type="button" class="accordion-button btn-info" data-bs-toggle="collapse" data-bs-target="#accordionTwo" aria-expanded="false" aria-controls="accordionTwo">The Cry of the Ocean </button>
                         </h2>
-                        <div id="accordionOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                        <div id="accordionTwo" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
                             <div class="accordion-body">
                                 <div class="row mb-12 g-6">
                                     <div class="col-md-6 col-lg-4">
                                         <div class="card">
                                             <div class="card-body">
                                                 <!-- <h5 class="card-title">green thumb farming </h5> -->
-                                                <img src="../reports/assets/img/green.jpg" width="300px">
                                                 <div class="demo-inline-spacing mt-4">
                                                     <div class="list-group">
-                                                        <a href="https://www.canva.com/design/DAGxqUJuSJU/HdfpLjSHrWbpW7D3L5WTjQ/edit" class="btn btn-dark">Video Link</a>
+                                                        <a href="https://www.canva.com/design/DAGxqUJuSJU/HdfpLjSHrWbpW7D3L5WTjQ/edit" class="btn btn-dark">View Project</a>
                                                     </div>
                                                 </div>
                                             </div>
