dcf5a5009bbf7c1a47e82c018d9730c131aabbd7
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-09-05 14:17:33 +0530

    Update header.php

--- school-reports/includes/header.php
+++ school-reports/includes/header.php
@@ -44,6 +44,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="manifest" href="../manifest.json">
 </head>
 
 <body>
