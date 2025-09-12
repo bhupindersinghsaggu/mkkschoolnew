a147c39ac9e9d1f5ae5420d68eee6d535083cb99
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-09-12 09:26:01 +0530

    Update function.php

--- school-reports/includes/function.php
+++ school-reports/includes/function.php
@@ -10,10 +10,10 @@ $total_teachers = $count_data['total_teachers'];
 
 
 // notebook check count from database 
-$count_query = "SELECT COUNT(teacher_id) as notebook_count FROM records";
-$count_result = mysqli_query($conn, $count_query);
-$count_data = mysqli_fetch_assoc($count_result);
-$notebook_count = $count_data['notebook_count'];
+// $count_query = "SELECT COUNT(teacher_id) as notebook_count FROM records";
+// $count_result = mysqli_query($conn, $count_query);
+// $count_data = mysqli_fetch_assoc($count_result);
+// $notebook_count = $count_data['notebook_count'];
 
 
 // class show count from database 
