c2b040a3be00545434ecadec8a8aef0b99d4207b
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-10-14 17:19:20 +0530

    Update css.php

--- school-reports2/includes/css.php
+++ school-reports2/includes/css.php
@@ -1,7 +1,7 @@
-<link rel="stylesheet" href="/mkkschool-new/assets/libs/jsvectormap/css/jsvectormap.min.css">
-<link rel="stylesheet" href="/mkkschool-new/assets/libs/jsvectormap/css/jsvectormap.min.css">
-<link href="/mkkschool-new/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-<link href="/mkkschool-new/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-<link href="/mkkschool-new/assets/css/app.min.css" rel="stylesheet" type="text/css" />
-<link href="/mkkschool-new/assets/css/custom.css" rel="stylesheet" type="text/css" />
-<link href="/mkkschool-new/assets/css/data-table.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="../assets/libs/jsvectormap/css/jsvectormap.min.css">
+<link rel="stylesheet" href="../assets/libs/jsvectormap/css/jsvectormap.min.css">
+<link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+<link href="../assets/css/app.min.css" rel="stylesheet" type="text/css" />
+<link href="../assets/css/custom.css" rel="stylesheet" type="text/css" />
+<link href="../assets/css/data-table.css" rel="stylesheet" type="text/css" />
