10cda631a8af11b9837a47ce98c52b80f28b2aec
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-10-14 17:30:46 +0530

    ss

--- school-reports2/includes/header.php
+++ school-reports2/includes/header.php
@@ -38,14 +38,14 @@ $username = isset($_SESSION['username']) ? $_SESSION['username'] : 'Guest';
     <meta content="" name="author" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <link rel="shortcut icon" href="assets/images/favicon.ico">
-    <!-- <link rel="stylesheet" href="assets/libs/jsvectormap/css/jsvectormap.min.css">
+    <link rel="stylesheet" href="assets/libs/jsvectormap/css/jsvectormap.min.css">
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />
     <link href="assets/css/custom.css" rel="stylesheet" type="text/css" />
     <link href="assets/css/data-table.css" rel="stylesheet" type="text/css" /> -->
     <!-- <link href="assets/css/style.css" rel="stylesheet" type="text/css" /> -->
-    <!-- jQuery (required for DataTables) -->
+    <!-- jQuery (required for DataTables)
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <!-- DataTables CSS -->
